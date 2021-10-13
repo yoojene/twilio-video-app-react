6da98f30dcd7a47401a9baeab9c5657e43b76558
yoojene <eugene.cross@gmail.com>
2021-10-13 19:18:02 +0100

    feat: stubbing out getVideoElementFromDialog hook and UI for dialog.

--- src/components/CaptureImageDialog/CaptureImageDialog.tsx
+++ src/components/CaptureImageDialog/CaptureImageDialog.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import {
-  DialogContent,
-  Divider,
-  Dialog,
-  DialogActions,
-  Button,
-  Theme,
-  DialogTitle,
-  makeStyles,
-} from '@material-ui/core';
+import { Divider, Dialog, DialogActions, Button, Theme, DialogTitle, makeStyles, Typography } from '@material-ui/core';
+import VideoTrack from '../VideoTrack/VideoTrack';
+import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
+import { LocalVideoTrack } from 'twilio-video';
+import useCaptureImageContext from '../../hooks/useCaptureImageContext/useCaptureImageContext';
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
@@ -43,15 +38,54 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export default function CaptureImageDialog({ open, onClose }: { open: boolean; onClose: () => void }) {
   const classes = useStyles();
+  const { localTracks } = useVideoContext();
+  const { getVideoElementFromDialog } = useCaptureImageContext();
+
+  const localVideoTrack = localTracks.find(track => track.kind === 'video') as LocalVideoTrack | undefined;
+
+  const captureImage = () => {
+    const video = getVideoElementFromDialog();
+    if (video) {
+      // console.log(video)
+      // let vid = video as HTMLMediaElement;
+      // console.log(vid.srcObject)
+      const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+      // canvas.style = 'display:none'
+
+      if (canvas) {
+        const ctx = canvas.getContext('2d');
+        canvas.width = 320;
+        canvas.height = 200;
+        ctx?.drawImage(video as CanvasImageSource, 0, 0, canvas.width, canvas.height);
+
+        // const data = canvas.toDataURL('image/png');
+        // const photo = document.getElementById('photo');
+        // photo?.setAttribute('src', data);
+      }
+    }
+  };
 
   return (
     <Dialog open={open} onClose={onClose} classes={{ paper: classes.paper }}>
       <DialogTitle>Capture Image</DialogTitle>
       <Divider />
-      <DialogContent className={classes.headline}>** Captured image placeholder **</DialogContent>
+      {/* <DialogContent className={classes.headline}>** Captured image placeholder **</DialogContent> */}
+      {localVideoTrack && (
+        <div>
+          <VideoTrack id={'capture-video'} isLocal track={localVideoTrack} />
+        </div>
+      )}
+      <Divider />
+      {/* <Typography variant="h6">
+         Captured Image
+      </Typography> */}
+      <canvas id="canvas"></canvas>
+      {/* <div>
+        <img id="photo" alt="The screen capture will appear in this box."/> 
+      </div> */}
       <Divider />
       <DialogActions>
-        <Button color="primary" variant="contained" className={classes.button} onClick={onClose}>
+        <Button color="primary" variant="contained" className={classes.button} onClick={captureImage}>
           Capture
         </Button>
       </DialogActions>
--- src/components/CaptureImageProvider/index.tsx
+++ src/components/CaptureImageProvider/index.tsx
@@ -1,8 +1,9 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 
 type CaptureImageContextType = {
   isCaptureImageDialogOpen: boolean;
   setIsCaptureImageDialogOpen: (isCaptureImageDialogOpen: boolean) => void;
+  getVideoElementFromDialog: () => HTMLElement | null;
 };
 
 export const CaptureImageContext = createContext<CaptureImageContextType>(null!);
@@ -10,8 +11,17 @@ export const CaptureImageContext = createContext<CaptureImageContextType>(null!)
 export const CaptureImageProvider: React.FC = ({ children }) => {
   const [isCaptureImageDialogOpen, setIsCaptureImageDialogOpen] = useState(false);
 
+  const getVideoElementFromDialog = useCallback(() => {
+    const video = document.getElementById('capture-video');
+    console.log(video);
+
+    return video;
+  }, []);
+
   return (
-    <CaptureImageContext.Provider value={{ isCaptureImageDialogOpen, setIsCaptureImageDialogOpen }}>
+    <CaptureImageContext.Provider
+      value={{ isCaptureImageDialogOpen, setIsCaptureImageDialogOpen, getVideoElementFromDialog }}
+    >
       {children}
     </CaptureImageContext.Provider>
   );
