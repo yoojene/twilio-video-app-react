1f345a65151fa0a0514112da86e523c5e6d21deb
yoojene <eugene.cross@gmail.com>
2021-12-15 17:35:43 +0000

    feat: remove DataTrack component and fold into RemoteImagePreview

--- src/components/RemoteImagePreview/RemoteImagePreview.tsx
+++ src/components/RemoteImagePreview/RemoteImagePreview.tsx
@@ -1,56 +1,71 @@
-import { DataStore } from '@aws-amplify/datastore';
+/* eslint-disable no-var */
 import { makeStyles } from '@material-ui/styles';
-import React, { useEffect, useState } from 'react';
-import { RemoteDataTrack } from 'twilio-video';
-import useCaptureImageContext from '../../hooks/useCaptureImageContext/useCaptureImageContext';
-import { Image } from '../../models';
+import React, { useEffect } from 'react';
 import { DataTrack as IDataTrack } from 'twilio-video';
 
-import DataTrack from '../DataTrack/DataTrack';
-import { defaultBase64Image } from './RemoteImagePreviewData';
-
 const useStyles = makeStyles(() => ({
-  photoPreview: {
-    '@media (max-width: 1600px)': {
-      width: '787px',
-    },
-  },
   canvasContainer: {
     width: '100%',
     textAlign: 'center',
   },
-  canvas: {
-    display: 'none',
-  },
-  photoContainer: {
-    width: '100%',
-    textAlign: 'center',
-    marginLeft: '16px',
-  },
 }));
-export default function RemoteImagePreview(props: { track: IDataTrack }) {
+export default function RemoteImagePreview({ track }: { track: IDataTrack }) {
   const classes = useStyles();
 
-  const { photoBase64, getImagesFromDataStore } = useCaptureImageContext();
-
   useEffect(() => {
-    getImagesFromDataStore();
-    const subscription = DataStore.observe(Image).subscribe(() => getImagesFromDataStore());
-    return () => subscription.unsubscribe();
-  }, []);
+    let count = 0;
+    var buf: Uint8ClampedArray;
+    let canvasWidthNum: number;
+    let canvasHeightNum: number;
+    const handleMessage = (event: ArrayBuffer | string) => {
+      if (typeof event === 'string' && event.startsWith('canvas')) {
+        // eslint-disable-next-line prefer-const
+        const canvasWidth = event.slice(event.indexOf(':') + 1, event.indexOf(','));
+        // eslint-disable-next-line prefer-const
+        const canvasHeight = event.slice(event.lastIndexOf(':') + 1);
+
+        canvasWidthNum = +canvasWidth;
+        canvasHeightNum = +canvasHeight;
+
+        return Promise.resolve([canvasWidthNum, canvasHeightNum]);
+      }
+      if (typeof event === 'string') {
+        // eslint-disable-next-line no-var
+        buf = new Uint8ClampedArray(parseInt(event));
+        count = 0;
+        console.log('Expecting a total of ' + buf.byteLength + ' bytes');
+        return;
+      }
+
+      const data = new Uint8ClampedArray(event);
+
+      buf.set(data, count);
+      count += data.byteLength;
+
+      if (count === buf.byteLength) {
+        console.log('Done can render photo now');
+        const remotecanvas = document.getElementById('remotecanvas') as HTMLCanvasElement;
+
+        const ctx = remotecanvas.getContext('2d');
+        remotecanvas.width = canvasWidthNum;
+        remotecanvas.height = canvasHeightNum;
+
+        const img = ctx?.createImageData(canvasWidthNum, canvasHeightNum);
+        img!.data.set(buf);
+        ctx?.putImageData(img!, 0, 0);
+      }
+    };
+    track.on('message', handleMessage);
+    return () => {
+      track.off('message', handleMessage);
+    };
+  }, [track]);
 
   return (
     <>
-      <DataTrack track={props.track} />
-      {/* <div className={classes.photoContainer}>
-        <img
-          id="photo"
-          src={photoBase64}
-          alt="Users screen capture"
-          className={classes.photoPreview}
-        // ref={imgRef}
-        />
-      </div> */}
+      <div className={classes.canvasContainer}>
+        <canvas id="remotecanvas"></canvas>
+      </div>
     </>
   );
 }
