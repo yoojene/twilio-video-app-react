182fd33692be8996c7f6bd2b91109fc12486f316
yoojene <eugene.cross@gmail.com>
2022-01-26 16:46:09 +0000

    feat: Add label to ToggleAudio button

--- src/components/Buttons/ToggleAudioButton/ToggleAudioButton.tsx
+++ src/components/Buttons/ToggleAudioButton/ToggleAudioButton.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-
-import Button from '@material-ui/core/Button';
 import MicIcon from '../../../icons/MicIcon';
 import MicOffIcon from '../../../icons/MicOffIcon';
-
 import useLocalAudioToggle from '../../../hooks/useLocalAudioToggle/useLocalAudioToggle';
 import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
-import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     iconContainer: {
       width: `${theme.iconButtonWidth}px`,
+    },
+    iconButton: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    label: {
+      fontSize: '12px',
     },
   })
 );
@@ -23,22 +27,29 @@ export default function ToggleAudioButton(props: { disabled?: boolean; className
   const hasAudioTrack = localTracks.some(track => track.kind === 'audio');
 
   return (
-    <Button
-      className={props.className}
-      onClick={toggleAudioEnabled}
-      disabled={!hasAudioTrack || props.disabled}
-      startIcon={
-        isAudioEnabled ? (
-          <div className={classes.iconContainer}>
-            <MicIcon />
-          </div>
+    <>
+      <IconButton
+        data-cy-audio-toggle
+        classes={{ label: classes.iconButton }}
+        onClick={toggleAudioEnabled}
+        disabled={!hasAudioTrack}
+      >
+        {isAudioEnabled ? (
+          <>
+            <div className={classes.iconContainer}>
+              <MicIcon />
+            </div>
+            <div className={classes.label}>Mic On</div>
+          </>
         ) : (
-          <div className={classes.iconContainer}>
-            <MicOffIcon />
-          </div>
-        )
-      }
-      data-cy-audio-toggle
-    ></Button>
+          <>
+            <div className={classes.iconContainer}>
+              <MicOffIcon />
+            </div>
+            <div className={classes.label}>Mic Off</div>
+          </>
+        )}
+      </IconButton>
+    </>
   );
 }
